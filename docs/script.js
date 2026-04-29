d79b7428da0bc537b6f9f3ac5d1a958bd1c70dff
Kenneth Jay <93532281+kenjpfernandez@users.noreply.github.com>
2026-04-29 14:40:26 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,23 +1,35 @@
+function login() {
+const user = document.getElementById("username").value;
+const pass = document.getElementById("password").value;
+const response = document.getElementById("loginResponse");
+
+if(user === "iciu_agent" && pass === "directive72") {
+window.location.href = "terminal.html";
+} else {
+response.innerHTML = "ACCESS DENIED";
+}
+}
+
 function checkCode() {
 const code = document.getElementById("codeInput").value.trim().toUpperCase();
 const response = document.getElementById("response");
 
 if(code === "0412") {
-response.innerHTML = "✔ Meeting Time Verified";
+response.innerHTML = "✔ Meeting Code Accepted";
 }
 else if(code === "19:00") {
-response.innerHTML = "✔ Calendar Conflict Logged";
+response.innerHTML = "✔ Timestamp Verified";
 }
 else if(code === "ACCT-7781") {
-response.innerHTML = "✔ Financial Anomaly Detected";
+response.innerHTML = "✔ Financial Record Flagged";
 }
 else if(code === "SILENTDIRECTIVE") {
-response.innerHTML = "⚠ ACCESS GRANTED: Opening Restricted Files...";
+response.innerHTML = "⚠ ACCESS GRANTED...";
 setTimeout(() => {
 window.location.href = "puzzles/day1_end.html";
-}, 2000);
+}, 1500);
 }
 else {
-response.innerHTML = "✖ INVALID CODE";
+response.innerHTML = "INVALID CODE";
 }
 }
