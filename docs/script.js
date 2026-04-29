71e82cbb16061e7ebcb9598fccbfff93a9ac6623
Kenneth Jay <93532281+kenjpfernandez@users.noreply.github.com>
2026-04-29 16:13:42 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -34,16 +34,16 @@ function checkCode() {
 
   unlockModule("mod2", "https://your-sharepoint-link/calendar", "[2] Calendar System");
 
-}
-
+} 
+  
   else if(code === "19:00" && !progress.code2) {
   progress.code2 = true;
   response.innerHTML = "✔ Timeline Verified → Finance Unlocked";
 
   unlockModule("mod3", "https://your-sharepoint-link/finance", "[3] Financial Records");
 
-}
-
+} 
+  
   else if(code === "ACCT-7781" && !progress.code3) {
   progress.code3 = true;
   response.innerHTML = "✔ Financial Record Verified → HR Unlocked";
@@ -51,7 +51,8 @@ function checkCode() {
   unlockModule("mod4", "https://your-sharepoint-link/hr", "[4] HR Database");
 
 }
-  } else if(code === "SILENTDIRECTIVE") {
+
+else if(code === "SILENTDIRECTIVE") {
 
     if(progress.code1 && progress.code2 && progress.code3) {
       response.innerHTML = "⚠ ACCESS GRANTED...";
