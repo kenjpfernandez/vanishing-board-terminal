d04d321c099e6d2c8ddb253def20b0552f97952f
Kenneth Jay <93532281+kenjpfernandez@users.noreply.github.com>
2026-04-29 17:07:35 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -32,7 +32,7 @@ function unlockModule(id, link, label) {
   const el = document.getElementById(id);
 
   if (!el) {
-    console.error("NOT FOUND:", id);
+    console.error("Module not found:", id);
     return;
   }
 
@@ -46,35 +46,35 @@ function checkCode() {
   const response = document.getElementById("response");
 
   if(code === "0412" && !progress.code1) {
-    progress.code1 = true;
-    response.innerHTML = "✔ Meeting Code Verified → Calendar Unlocked";
+  progress.code1 = true;
+  response.innerHTML = "✔ Meeting Code Verified → Calendar Unlocked";
 
-    unlockModule(
-      "mod2",
-      "https://thermofisher-my.sharepoint.com/:f:/p/kennethjay_fernandez/IgD1SblGIzxNQ6TZ1Foftn-cAcBh_9Al9knRBjzr4f9mEYg?e=MpCGgl",
-      "[2] Calendar System"
-    );
+  unlockModule(
+    "mod2",
+    "https://thermofisher-my.sharepoint.com/:f:/p/kennethjay_fernandez/IgD1SblGIzxNQ6TZ1Foftn-cAcBh_9Al9knRBjzr4f9mEYg?e=MpCGgl",
+    "[2] Calendar System"
+  );
 
-  } else if(code === "19:00" && !progress.code2) {
-    progress.code2 = true;
-    response.innerHTML = "✔ Timeline Conflict Logged → Finance Unlocked";
+} else if(code === "19:00" && !progress.code2) {
+  progress.code2 = true;
+  response.innerHTML = "✔ Timeline Conflict Logged → Finance Unlocked";
 
-    unlockModule(
-      "mod3",
-      "https://thermofisher-my.sharepoint.com/:f:/p/kennethjay_fernandez/IgDgTUmNrPITSrHUUBdY9iJSAQYPRUNoMTiKlcXH1S1AHBs?e=efYdZj",
-      "[3] Financial Records"
-    );
+  unlockModule(
+    "mod3",
+    "https://thermofisher-my.sharepoint.com/:f:/p/kennethjay_fernandez/IgDgTUmNrPITSrHUUBdY9iJSAQYPRUNoMTiKlcXH1S1AHBs?e=efYdZj",
+    "[3] Financial Records"
+  );
 
-  } else if(code === "ACCT-7781" && !progress.code3) {
-    progress.code3 = true;
-    response.innerHTML = "✔ Financial Anomaly Flagged → HR Unlocked";
+} else if(code === "ACCT-7781" && !progress.code3) {
+  progress.code3 = true;
+  response.innerHTML = "✔ Financial Anomaly Flagged → HR Unlocked";
 
-    unlockModule(
-      "mod4",
-      "https://thermofisher-my.sharepoint.com/:f:/p/kennethjay_fernandez/IgDJIhaZCRjAQJWyCGulDOH9Aa3V19mCeqF7ehQZgKj4VDI?e=QdffW7",
-      "[4] HR Database"
-    );
-
+  unlockModule(
+    "mod4",
+    "https://thermofisher-my.sharepoint.com/:f:/p/kennethjay_fernandez/IgDJIhaZCRjAQJWyCGulDOH9Aa3V19mCeqF7ehQZgKj4VDI?e=QdffW7",
+    "[4] HR Database"
+  );
+    
   } else if(code === "SILENTDIRECTIVE") {
 
     if(progress.code1 && progress.code2 && progress.code3) {
